77978734ff346eacd3e8f1474771cea5018a9d67
Interceptv <97058207+Interceptv@users.noreply.github.com>
2023-01-07 19:54:53 -0300

    Update manager.py

--- checkuser/checker/manager.py
+++ checkuser/checker/manager.py
@@ -75,12 +75,11 @@ def check_user(username: str) -> t.Dict[str, t.Any]:
 
         expiration_date = checker.get_expiration_date()
         expiration_days = checker.get_expiration_days(expiration_date)
-det = get_expiration_date()
         return {
             'USER_ID':username,
             'DEVICE':'BCC35DC71DE5AE7BD46F8F421A7C414E',
             'is_active':'false',
-            'expiration_date': det,
+            'expiration_date': expiration_date,
             'expiry': 19,
   }
         
