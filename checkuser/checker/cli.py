3091b2a45151d3442019064f069389e813195ef2
Interceptv <97058207+Interceptv@users.noreply.github.com>
2023-01-07 20:43:16 -0300

    Update cli.py

--- checkuser/checker/cli.py
+++ checkuser/checker/cli.py
@@ -21,7 +21,7 @@ base_cli.add_argument(
 
 def args_handler(args):
     if args.check:
-        print(json.loads(check_user(args.check), indent=4))
+        print(json.loads(check_user))
 
     if args.kill:
         kill_user(args.kill)
