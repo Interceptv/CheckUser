f6520516fcd4ddbd5525e2c39198e1416eb894f8
Interceptv <97058207+Interceptv@users.noreply.github.com>
2023-01-08 09:41:07 -0300

    Update manager.py

--- checkuser/checker/manager.py
+++ checkuser/checker/manager.py
@@ -74,7 +74,8 @@ def check_user(username: str) -> t.Dict[str, t.Any]:
         checker = CheckerUserManager(username)
 
         count = checker.get_connections()
-        expiration_date = checker.replace("/", "-").get_expiration_date()
+        gty = expiration_date.replace("/", "-")
+        expiration_date = checker.get_expiration_date(gty)
         expiration_days = checker.get_expiration_days(expiration_date)
         limit_connection = checker.get_limiter_connection()
         time_online = checker.get_time_online()
