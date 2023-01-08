5eb27bb4e4b0756624f8daddd41fb1883d8fc073
Interceptv <97058207+Interceptv@users.noreply.github.com>
2023-01-07 21:13:06 -0300

    Update manager.py

--- checkuser/checker/manager.py
+++ checkuser/checker/manager.py
@@ -15,13 +15,7 @@ class CheckerUserManager:
 
     def get_expiration_date(self) -> t.Optional[str]:
         try:
-            chage = subprocess.Popen(
-                ('chage', '-l', self.username), stdout=subprocess.PIPE)
-            grep = subprocess.Popen(
-                ('grep', 'Account expires'), stdin=chage.stdout, stdout=subprocess.PIPE)
-            cut = subprocess.Popen(
-                'cut -d : -f2'.split(), stdin=grep.stdout, stdout=subprocess.PIPE)
-            output = cut.communicate()[0].strip().decode()
+           
 
             if not output or output == 'never':
                 return None
